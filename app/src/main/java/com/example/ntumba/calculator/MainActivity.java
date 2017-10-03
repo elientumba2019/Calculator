a618c7b350a152fab8907a22d9e0b2bfc5c935d0
Elie nsampi ntumba <elientumba@live.fr>
2017-10-03 19:13:12 +0800

    Implementation of the addition

--- app/src/main/java/com/example/ntumba/calculator/MainActivity.java
+++ app/src/main/java/com/example/ntumba/calculator/MainActivity.java
@@ -15,6 +15,18 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private double firstValue , secondValue;
+    private boolean resetValue;
+    private int lastKey;
+    private int lastOperation;
+
+    private static final int DIGIT = 0;
+    private static final int EQUALS = 1;
+    private static final int PLUS = 2;
+
+
+
     //getting a reference to the screen
     @BindView(R.id.display) TextView result;
 
@@ -33,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
      * @param number
      */
     private void addDigit(int number){
-        final String onScreen = GetElemenOnScreen();
+        final String onScreen = GetElementOnScreen();
         final String newVar = getValue(onScreen + number);
         result.setText(newVar);
     }
@@ -45,9 +57,36 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
     @NonNull
-    private String GetElemenOnScreen() {
+    private String GetElementOnScreen() {
+        resetValueIfNeeded();
         return result.getText().toString().trim();
     }
+
+
+
+    /**
+     * resets the value on screen
+     * if necessary
+     */
+    private void resetValueIfNeeded(){
+        if(resetValue){
+            result.setText("0");
+        }
+
+        resetValue = false;
+    }
+
+
+    /**
+     * returns the numbers on Screen
+     * using a double representation
+     * @return
+     */
+    private double getDisplayedNumberAsDouble(){
+        return Double.parseDouble(result.getText().toString().trim());
+    }
+
+
 
 
     /**
@@ -56,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
     private String getValue(String newValue){
-        double value = Double.parseDouble(newValue);
+        final double value = Double.parseDouble(newValue);
         return  formatDouble(value);
     }
 
@@ -75,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else{
-            final DecimalFormat format = new DecimalFormat("0.0############");
+            final DecimalFormat format = new DecimalFormat("0.0#############");
             return format.format(d);
         }
     }
@@ -91,6 +130,8 @@ public class MainActivity extends AppCompatActivity {
             R.id.btn_3, R.id.btn_4, R.id.btn_5,
             R.id.btn_6, R.id.btn_7, R.id.btn_8, R.id.btn_9})
     public void digitClicked(View view){
+
+        lastKey = DIGIT;
 
         switch (view.getId()){
 
@@ -127,16 +168,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     /**
      * actions to be taken when the decimal point is clicked on
      */
     @OnClick(R.id.btn_decimal)
     public void decimalClicked(){
 
-        String value = GetElemenOnScreen();
+        String value = GetElementOnScreen();
         if(!value.contains(".")){
             value += ".";
             result.setText(value);
+            lastKey = DIGIT;
         }
     }
 
@@ -148,10 +191,81 @@ public class MainActivity extends AppCompatActivity {
      */
     @OnClick(R.id.btn_0)
     public void zeroClicked(){
-        String value = GetElemenOnScreen();
+        String value = GetElementOnScreen();
         if(!value.isEmpty() && !value.equals("0")){
             value += "0";
             result.setText(value);
+            lastKey = DIGIT;
+        }
+    }
+
+
+    /**
+     * adds two numbers and displays
+     * the corresponding result on screen
+     * making the result the new first number
+     * to be added in the futures or
+     * used in any other operation
+     */
+    public void addNumbers(){
+        final double resultValue = firstValue + secondValue;
+        result.setText(formatDouble(resultValue));
+        firstValue = resultValue;
+    }
+
+
+
+    /**
+     * actions to tbe performed when the plus buttom is clicked;
+     */
+    @OnClick(R.id.btn_plus)
+    public void onPlusClicked(){
+        resetValue = true;
+        lastOperation = PLUS;
+
+        if(lastKey != DIGIT){
+            lastKey = PLUS;
+            return;
+        }
+
+
+        secondValue = getDisplayedNumberAsDouble();
+        addNumbers();
+        lastKey = PLUS;
+    }
+
+
+    /**
+     * actions to to be performed
+     * when the equal key is clicked
+     */
+    @OnClick(R.id.btn_equals)
+    public void onEqualClicked(){
+
+        if(lastKey == EQUALS){
+            handleEqual();
+            return;
+        }
+
+
+        if(lastKey != DIGIT){
+            return;
+        }
+
+
+
+        secondValue = getDisplayedNumberAsDouble();
+        handleEqual();
+        lastKey = EQUALS;
+    }
+
+
+    /**
+     * handling the equal button
+     */
+    private void handleEqual(){
+        if(lastOperation == PLUS){
+            addNumbers();
         }
     }
 }
